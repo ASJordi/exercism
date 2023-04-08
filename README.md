86b7d41f72c8525f779175031ac834c3d3854023
Jordi Ayala <jord7as@gmail.com>
2023-04-08 07:40:24 -0600

    feat(java): :sparkles: add ex-009

--- README.md
+++ README.md
@@ -74,6 +74,7 @@ Cada lenguaje tiene su propia carpeta en el repositorio. Dentro de cada carpeta,
         <li><a href="java/src/main/java/easy/MicroBlog.java">Micro Blog</a></li>
         <li><a href="java/src/main/java/easy/ArmstrongNumbers.java">Armstrong Numbers</a></li>
         <li><a href="java/src/main/java/easy/SqueakyClean.java">Squeaky Clean</a></li>
+        <li><a href="java/src/main/java/easy/Hamming.java">Hamming</a></li>
       </ol>
     </details>
     <details>
---
+++ java/src/main/java/easy/Hamming.java
@@ -0,0 +1,27 @@
+package easy;
+
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class Hamming {
+
+    private final int hammingDistance;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        String exceptionMessage = "leftStrand and rightStrand must be of equal length.";
+
+        if (leftStrand.length() != rightStrand.length()) {
+            if (leftStrand.isEmpty()) exceptionMessage = "left strand must not be empty.";
+            if (rightStrand.isEmpty()) exceptionMessage = "right strand must not be empty.";
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+
+        IntPredicate areNotEqual = index -> leftStrand.charAt(index) != rightStrand.charAt(index);
+        hammingDistance = (int) IntStream.range(0, leftStrand.length()).filter(areNotEqual).count();
+    }
+
+    int getHammingDistance() {
+        return hammingDistance;
+    }
+
+}
